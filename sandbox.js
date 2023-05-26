0848236819d7c818760386a25e31f36727ab4e24
Alfie Collins <alfiecollins44@gmail.com>
2023-05-26 16:26:41 +0100

    initial stuff

---
+++ sandbox.js
@@ -0,0 +1,9 @@
+var character = 'alfie';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+inputs.forEach(function (input) {
+    console.log(inputs);
+});
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,12 @@
+const character = 'alfie';
+
+console.log(character);
+
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  console.log(inputs);
+});
+
+inputs.forEach(input=> { console.log(input)
+});
