963a7fb81d076c57f705a6169a849e816cb1194f
Alfie Collins <alfiecollins44@gmail.com>
2023-07-10 17:23:27 +0100

    explicit types

--- sandbox.ts
+++ sandbox.ts
@@ -1,4 +1,4 @@
-let character = 'Alfie'
+/* let character = 'Alfie'
 let age = 30;
 let isBlackBelt = false;
 
@@ -8,7 +8,81 @@ character = 'Bob';
 isBlackBelt = true;
 
 const circ = (diameter: number) => {
-  return diameter * Math.PI
+  return diameter * Math.PI;
 } 
 
-console.log(circ(7.5))
+console.log(circ(7.5)); */
+
+//arrays
+let names = ['luigi', 'mario', 'yoshi'];
+
+names.push('toad');
+//names.push(3) -> This will not work because wrong type
+
+let numbers = [10, 20, 30, 40]
+
+//numbers.push('bob'); -> wrong type
+
+let mixed = ['ken', 4, 'chun-li', 8, 9]; //-> now we can add either strings or numbers to this array.
+
+mixed.push('ryu');
+mixed.push(10);
+
+//objects
+let ninja = {
+  name: 'mario',
+  belt: 'black',
+  age: 30
+};
+
+ninja.age = 40;
+ninja.name = 'ryu';
+//ninja.age = '30' -> wrong type for object property 'age'
+//ninja.skills = ['fighting', 'sneaking']; -> cannot add new property to the object
+
+ninja = {
+  name: 'yoshi',
+  belt: 'orange',
+  age: 40
+};
+
+//explicit types
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
+
+age = 30;
+
+//arrays
+let ninjas: string[] = [];
+
+ninjas.push('alfie');
+
+//union types
+let mixedTwo: (string|number|boolean)[] = [];
+
+mixedTwo.push('hello');
+mixedTwo.push(20);
+mixedTwo.push(false);
+
+let uid: string|number;
+uid = 'hello';
+uid = 123;
+
+//objects
+
+let ninjaOne: object;
+ninjaOne = {name: 'yoshi', age: 30};
+ninjaOne = [];
+
+let ninjaTwo: {
+  name: string,
+  age: number,
+  beltColour: string;
+};
+
+ninjaTwo = {
+  name: 'Alfie',
+  age: 20,
+  beltColour: 'black'
+}
